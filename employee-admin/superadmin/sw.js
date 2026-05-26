4d5c5c417f7f0d56b5df7a568484c55b17e4a584
unknown <chayfree86@gmail.com>
2026-05-26 16:56:04 +0530

    bbb

--- employee-admin/superadmin/sw.js
+++ employee-admin/superadmin/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'employee-superadmin-v2';
+const CACHE_NAME = 'employee-superadmin-v3';
 const APP_SHELL = [
   '/employee-admin/superadmin/index.html',
   '/employee-admin/superadmin/manifest.webmanifest',
